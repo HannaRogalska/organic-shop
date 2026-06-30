2357a340c418e5940cdba895d47a6a6018b3a4c4
Hanna Rogalska <hanrogalska@gmail.com>
2026-06-30 15:13:00 +0200

    chore: initialize neonand redis  connection client

--- src/shared/api/db/index.ts
+++ src/shared/api/db/index.ts
@@ -8,10 +8,27 @@ if (process.env.NODE_ENV === 'development') {
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is missing!');
 }
+declare global {
+  var globalDbPool: Pool | undefined;
+}
+let pool: Pool;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 10,
-  idleTimeoutMillis: 30000,
-});
+if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 15,
+    idleTimeoutMillis: 15000,
+    connectionTimeoutMillis: 5000,
+  });
+} else {
+  if (!globalThis.globalDbPool) {
+    globalThis.globalDbPool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      max: 5,
+      idleTimeoutMillis: 30000,
+    });
+  }
+  pool = globalThis.globalDbPool;
+}
+
 export const db = drizzle({ client: pool });
