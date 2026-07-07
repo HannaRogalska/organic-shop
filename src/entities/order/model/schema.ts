638a9aa1d86fb2a908355d9009b223bc6579eb78
Hanna Rogalska <hanrogalska@gmail.com>
2026-07-07 16:20:37 +0200

    db: create orders and order_items table schemas

--- src/entities/order/model/schema.ts
+++ src/entities/order/model/schema.ts
@@ -4,19 +4,23 @@ import { productsTable } from '@/entities/product/model/schema';
 
 export const orderStatusEnum = pgEnum('order_status', ['pending', 'paid', 'shipped']);
 
-export const ordersTable = pgTable('orders', {
-  id: uuid().defaultRandom().primaryKey(),
-  userId: uuid()
-    .references(() => usersTable.id, { onDelete: 'restrict' })
-    .notNull(),
-  status: orderStatusEnum().default('pending').notNull(),
-  totalAmount: numeric({ precision: 10, scale: 2 }).notNull(),
-  createdAt: timestamp().defaultNow().notNull(),
-  updatedAt: timestamp()
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-});
+export const ordersTable = pgTable(
+  'orders',
+  {
+    id: uuid().defaultRandom().primaryKey(),
+    userId: uuid()
+      .references(() => usersTable.id, { onDelete: 'restrict' })
+      .notNull(),
+    status: orderStatusEnum().default('pending').notNull(),
+    totalAmount: numeric({ precision: 10, scale: 2 }).notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp()
+      .defaultNow()
+      .$onUpdate(() => new Date())
+      .notNull(),
+  },
+  (table) => [index('orders_user_id_idx').on(table.userId)]
+);
 
 export const orderItemsTable = pgTable(
   'order_items',
