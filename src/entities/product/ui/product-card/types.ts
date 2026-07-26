107d2f22b5ff2f74a36b031c0569d49573779517
Hanna Rogalska <hanrogalska@gmail.com>
2026-07-26 11:17:17 +0200

    feat(product): add product card types

---
+++ src/entities/product/ui/product-card/types.ts
@@ -0,0 +1,17 @@
+export type ProductCardProduct = {
+  id: string;
+  title: string;
+  image: string;
+  price: number | string;
+  rating?: number | string | null;
+};
+
+export type ProductCardProps = {
+  product: ProductCardProduct;
+  currency?: string;
+  locale?: string;
+  className?: string;
+  onAddToCart?: (product: ProductCardProduct) => void;
+  onQuickView?: (product: ProductCardProduct) => void;
+  onAddToWishlist?: (product: ProductCardProduct) => void;
+};
